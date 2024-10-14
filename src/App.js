131821c573cb809366de264c50140ac62ba18027
xjwsx <wlstmdghs@naver.com>
2024-10-14 23:27:44 +0900

    feat: filter 기능, 선택 삭제 기능, 배경 색,todolist 수정 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import ToDoList from "./ToDoList";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: linear-gradient(to bottom right, #ff0000, #800080);
+    height: 100vh;
+  }
+`;
 
 function App() {
   return (
-    <div>
+    <>
+      <GlobalStyle />
       <ToDoList />
-    </div>
+    </>
   );
 }
 
